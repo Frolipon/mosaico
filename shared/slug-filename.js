1b75f1e0a6cea4ed27fe3e584cdbbf83e401351d
Hiswe <hiswehalya@gmail.com>
2016-12-16 18:47:35 +0100

    SLUG – add log (can't reproduce – fix #71)

--- shared/slug-filename.js
+++ shared/slug-filename.js
@@ -3,7 +3,7 @@
 var getSlug = require('speakingurl')
 
 // take care of slugging everything BUT the file extension
-// keeping this file as minimal as possible -> used in front-end…
+// keeping this file as minimal as possible -> used in front-end (editor)
 var extReg  = /\.[0-9a-z]+$/
 
 function slugFilename(name) {
@@ -12,7 +12,10 @@ function slugFilename(name) {
   // https://github.com/goodenough/mosaico/issues/71
   // Haven't been able to reproduce but secure it
   var isValidName = extReg.test(name)
-  if (!isValidName) return false
+  if (!isValidName) {
+    console.log('[SLUGFILENAME] impossible to slug', name)
+    return false
+  }
 
   var ext         = extReg.exec(name)[0]
   fileName        = fileName.replace(ext, '')
