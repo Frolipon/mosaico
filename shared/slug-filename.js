0663b2b6be8ffd8db1cee41381fb003685ff7fd0
Hiswe <hiswehalya@gmail.com>
2017-05-12 08:34:00 +0700

    UPLOAD – fix files with camelCase

--- shared/slug-filename.js
+++ shared/slug-filename.js
@@ -4,10 +4,12 @@ var getSlug = require('speakingurl')
 
 // take care of slugging everything BUT the file extension
 // keeping this file as minimal as possible -> used in front-end (editor)
-var extReg  = /\.[0-9a-z]+$/
+var extReg  = /\.[0-9a-zA-Z]+$/
 
 function slugFilename(name) {
-  var fileName      = name.toLowerCase().trim()
+  // keep the fileName with uppercases
+  // wireframes preview's images are camelCased
+  var fileName      = name.trim()
   // It seems that some files came here without extension O_O'
   // https://github.com/goodenough/mosaico/issues/71
   // Haven't been able to reproduce but secure it
